17773c74e5b19cee9e493de91c7cb8e6c8f95207
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2015-01-01 10:32:57 +0400

    Amend Task definition
    Tasks have new attributes which specify whether they are
    repeating and, if so, with what period, whether it is useful to
    partially complete them and the maximum number of time chunks into
    which they can be divided. Tasks also have a UID associated with
    them.
    
    Additionally, the time property of the class has been changed back
    to a change_time method since the level of handling required on the
    possible input parameters was beyond what is acceptable for an
    attribute.

--- src/thread/task.py
+++ src/thread/task.py
@@ -1,6 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+This module contains classes representing different types of tasks.
+
+Module structure:
+- Task
+  - Event
+  - Assignment
+  - RecurrentTask
+
+"""
 __author__ = 'dibyo'
 
 from datetime import datetime, timedelta
+import uuid
+import json
 
 
 class Task(object):
@@ -13,15 +27,30 @@ class Task(object):
                  name: str,
                  start_time: datetime=None,
                  end_time: datetime=None,
-                 importance: int=5):
+                 importance: int=5,
+                 repeating: bool=False,
+                 period: timedelta=None,
+                 partial_completion: bool=False,
+                 max_divisions: int=-1):
         """
         :param name: the name of the task
         :param start_time: the start time of the task
         :param end_time: the end time of the task
+        :param importance: the importance given to the task
+        :param repeating: whether the task is repeating
+        :param period: the period of repetition of the task
+        :param partial_completion: whether partial completion is useful
+        :param max_divisions: the maximum number of non-consecutive timechunks
+            that the task can be divided into
         """
+        self.uid = uuid.uuid4()
         self.name = name
         self.start_time = self.end_time = None
-        self.importance = importance
+        self._importance = importance
+        self.repeating = repeating
+        self.period = period
+        self.partial_completion = partial_completion
+        self.max_divisions = max_divisions
 
         if start_time:
             self.change_time(start_time, end_time)
@@ -32,9 +61,15 @@ class Task(object):
         """
         return "Task {0.name} in {0.start_time}-{0.end_time}".format(self)
 
-    def set_time(self,
-                 new_start_time: datetime,
-                 new_end_time: datetime=None):
+    class TaskJSONDecoder(json.JSONDecoder):
+        pass
+
+    class TaskJSONEncoder(json.JSONEncoder):
+        pass
+
+    def change_time(self,
+                    new_start_time: datetime,
+                    new_end_time: datetime=None):
         """
         Change the start and (optionally) end time of a task.
 
@@ -50,16 +85,25 @@ class Task(object):
         self.start_time = new_start_time
         self.end_time = new_end_time
 
-    def set_importance(self, new_importance: int):
+    @property
+    def importance(self):
+        return self._importance
+
+    @importance.setter
+    def importance(self, new_importance: int):
         """
         Change the importance of a task
 
         :param new_importance: the new importance of the task
         """
-        if new_importance >= 0 and new_importance < 10:
-            self.importance = new_importance
+        if 0 <= new_importance < 10:
+            self._importance = new_importance
         else:
             raise Exception("Invalid importance for task. ")
+
+    @importance.deleter
+    def importance(self):
+        del self._importance
 
 
 class Event(Task):
@@ -72,13 +116,25 @@ class Event(Task):
                  name: str,
                  start_time: datetime,
                  end_time: datetime,
-                 importance: int=5):
+                 importance: int=5,
+                 repeating: bool=False,
+                 period: timedelta=None,
+                 partial_completion: bool=False,
+                 max_divisions: int=-1):
         """
         :param name: the name of the event
         :param start_time: the start time of the event
         :param end_time: the end time of the event
+        :param importance: the importance given to the task
+        :param repeating: whether the task is repeating
+        :param period: the period of repetition of the task
+        :param partial_completion: whether partial completion is useful
+        :param max_divisions: the maximum number of non-consecutive timechunks
+            that the task can be divided into
         """
-        super(Event, self).__init__(name, start_time, end_time, importance)
+        super(Event, self).__init__(name, start_time, end_time, importance,
+                                    repeating, period, partial_completion,
+                                    max_divisions)
 
     def __str__(self):
         """
@@ -97,14 +153,29 @@ class Assignment(Task):
                  name: str,
                  deadline: datetime,
                  expected_duration: timedelta,
-                 importance: int=5):
+                 importance: int=5,
+                 repeating: bool=False,
+                 period: timedelta=None,
+                 partial_completion: bool=False,
+                 max_divisions: int=-1):
         """
         :param name: the name of the event
         :param deadline: the time at which the assignment is due
         :param expected_duration: the expected time required to
             complete the assignment
+        :param importance: the importance given to the task
+        :param repeating: whether the task is repeating
+        :param period: the period of repetition of the task
+        :param partial_completion: whether partial completion is useful
+        :param max_divisions: the maximum number of non-consecutive timechunks
+            that the task can be divided into
         """
-        super(Assignment, self).__init__(name, importance=importance)
+        super(Assignment, self).__init__(name,
+                                         importance=importance,
+                                         repeating=repeating,
+                                         period=period,
+                                         partial_completion=partial_completion,
+                                         max_divisions=max_divisions)
         self.deadline = deadline
         self.expected_duration = expected_duration
 
@@ -114,9 +185,9 @@ class Assignment(Task):
         """
         return "Assignment {0.name} by {0.deadline}".format(self)
 
-    def set_time(self,
-                 new_start_time: datetime,
-                 new_end_time: datetime=None):
+    def change_time(self,
+                    new_start_time: datetime,
+                    new_end_time: datetime=None):
         """
         Change the start and (optionally) end time of a assignment.
         Also change the expected duration of the assignment if
